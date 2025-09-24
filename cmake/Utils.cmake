001c76f551bfd52b97f656c806014e5adca8bcf6
Eldar Salmanow <eldar.salm@gmail.com>
2025-09-24 23:36:49 +0300

    Some upgradings

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,16 +1,59 @@
-function(add_task TASK_NUM)
-    cmake_parse_arguments(ARG "" "" "SOURCES" ${ARGN})
+function(setup_project)
+    include(FetchContent)
 
-    set(TASK${TASK_NUM}_SOURCES)
-    foreach(SOURCE ${ARG_SOURCES})
+    FetchContent_Declare(
+            googletest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG        v1.17.0
+    )
+    FetchContent_MakeAvailable(googletest)
+
+    add_library(GTest::GTest
+            INTERFACE
+            IMPORTED
+    )
+
+    target_link_libraries(GTest::GTest
+            INTERFACE
+            gtest_main
+    )
+
+    enable_testing()
+endfunction()
+
+function(add_task TASK_NUM)
+    # Parsing arguments
+
+    cmake_parse_arguments(ARG "" "" "CLI_SOURCES;LIB_SOURCES;TEST_SOURCES" ${ARGN})
+
+    set(TASK${TASK_NUM}_CLI_SOURCES)
+    foreach(SOURCE ${ARG_CLI_SOURCES})
         list(APPEND
-                TASK${TASK_NUM}_SOURCES
+                TASK${TASK_NUM}_CLI_SOURCES
+                ${CLI_DIR}/${SOURCE}
+        )
+    endforeach()
+
+    set(TASK${TASK_NUM}_LIB_SOURCES)
+    foreach(SOURCE ${ARG_LIB_SOURCES})
+        list(APPEND
+                TASK${TASK_NUM}_LIB_SOURCES
                 ${SOURCE_DIR}/task${TASK_NUM}/${SOURCE}
         )
     endforeach()
 
+    set(TASK${TASK_NUM}_TEST_SOURCES)
+    foreach(SOURCE ${ARG_TEST_SOURCES})
+        list(APPEND
+                TASK${TASK_NUM}_TEST_SOURCES
+                ${TESTS_DIR}/${SOURCE}
+        )
+    endforeach()
+
+    # Adding library
+
     add_library(task${TASK_NUM}_lib
-            ${TASK${TASK_NUM}_SOURCES}
+            ${TASK${TASK_NUM}_LIB_SOURCES}
     )
 
     target_include_directories(task${TASK_NUM}_lib
@@ -18,8 +61,20 @@ function(add_task TASK_NUM)
             ${INCLUDE_DIR}
     )
 
+    target_link_libraries(task${TASK_NUM}_lib
+            PRIVATE
+            m
+    )
+
+    target_compile_options(task${TASK_NUM}_lib
+            PRIVATE
+            -Wall -Werror -Wextra -Wpedantic
+    )
+
+    # Adding CLI app
+
     add_executable(task${TASK_NUM}
-            ${CLI_DIR}/task${TASK_NUM}.c
+            ${TASK${TASK_NUM}_CLI_SOURCES}
     )
 
     target_include_directories(task${TASK_NUM}
@@ -31,11 +86,16 @@ function(add_task TASK_NUM)
             PRIVATE
             task${TASK_NUM}_lib
     )
-endfunction()
 
-function(add_task_test TASK_NUM)
+    target_compile_options(task${TASK_NUM}
+            PRIVATE
+            -Wall -Werror -Wextra -Wpedantic
+    )
+
+    # Adding tests
+
     add_executable(task${TASK_NUM}_test
-            task${TASK_NUM}_test.cpp
+            ${TASK${TASK_NUM}_TEST_SOURCES}
     )
 
     target_include_directories(task${TASK_NUM}_test
